3c7f807fb10f03627204235cd3785fdbf2726a6f
yk3823 <yk3823@gmail.com>
2019-09-09 22:06:58 +0300

    first commit02

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+from django.contrib.auth.models import User
+
+# Register your models here.
+admin.site.register(UserProfileInfo)
+
+
